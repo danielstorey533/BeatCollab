6d4d8580125bc5f57ce8d3322dd3f79a093ad978
Maks Suvajev <maksimilian.suvajev@ucdconnect.ie>
2020-05-16 16:05:17 +0100

    Initial track playing functionality added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -10,15 +12,42 @@ function App() {
       setCurrentTime(data.time);
     });
   }, []);
+  
+  let audio = new Audio("http://localhost:3000/BEAT.wav")
+  
+  const start = () => {
+	  
+	  audio.play()
+	  
+  } 
+  
+  const stop = () => {
+	  
+	  audio.pause()
+	  audio.currentTime = 0
+	  
+  }
+  
+  const pause = () => {
+	  
+	  audio.pause()
+	  
+  }
 
   return (
     <div className="App">
+	
       <header className="App-header">
-
-
-
         <p>The current time is {currentTime}.</p>
       </header>
+	  
+	  <div>
+		<button onClick={start}>Play</button>
+		<button onClick={pause}>Pause</button>
+		<button onClick={stop}>Stop</button>
+	  </div>
+	 
+	  
     </div>
   );
 }
